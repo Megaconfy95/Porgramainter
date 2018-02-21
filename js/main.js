58b6159eafe724f6d0a93df57f6b1d56b105a6f3
Megaconfy95 <jorge.tejeda@alumnos.udg.mx>
2018-02-20 20:29:47 -0600

    modificacion

--- js/main.js
+++ js/main.js
@@ -8,8 +8,12 @@ function preload() {
     game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 
 }
+var score = 0;
+var scoreText;
+var cursors;
 var player;
 var platforms;
+var stars;
 
 function create() {
 
@@ -56,12 +60,87 @@ function create() {
     player.animations.add('left', [0, 1, 2, 3], 10, true);
     player.animations.add('right', [5, 6, 7, 8], 10, true);
 
+
+//  Our controls.
+    cursors = game.input.keyboard.createCursorKeys();
+
+
+    //  Finally some stars to collect
+    stars = game.add.group();
+
+    //  We will enable physics for any star that is created in this group
+    stars.enableBody = true;
+
+    //  Here we'll create 12 of them evenly spaced apart
+    for (var i = 0; i < 12; i++)
+    {
+        //  Create a star inside of the 'stars' group
+        var star = stars.create(i * 70, 0, 'star');
+
+        //  Let gravity do its thing
+        star.body.gravity.y = 300;
+
+        //  This just gives each star a slightly random bounce value
+        star.body.bounce.y = 0.7 + Math.random() * 0.2;
+    }
+    //  The score
+    scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+
 }
 
 function update() {
 
     //  Collide the player and the stars with the platforms
     game.physics.arcade.collide(player, platforms);
+    game.physics.arcade.collide(stars, platforms);
+
+    //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
+    game.physics.arcade.overlap(player, stars, collectStar, null, this);
+
+
+
+    //  Reset the players velocity (movement)
+    player.body.velocity.x = 0;
+
+    if (cursors.left.isDown)
+    {
+        //  Move to the left
+        player.body.velocity.x = -150;
+
+        player.animations.play('left');
+    }
+    else if (cursors.right.isDown)
+    {
+        //  Move to the right
+        player.body.velocity.x = 150;
+
+        player.animations.play('right');
+    }
+    else
+    {
+        //  Stand still
+        player.animations.stop();
+
+        player.frame = 4;
+    }
+
+    //  Allow the player to jump if they are touching the ground.
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        player.body.velocity.y = -350;
+    }
+}
+
+
+function collectStar (player, star) {
+
+    // Removes the star from the screen
+    star.kill();
+
+
+    //  Add and update the score
+    score += 10;
+    scoreText.text = 'Score: ' + score;
 
 }
 
